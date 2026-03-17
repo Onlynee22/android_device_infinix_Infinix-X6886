ef85c19bdb9982ffe1fb4aaca0e2d89b905aefb4
Onlynee22 <115428631+Onlynee22@users.noreply.github.com>
2026-03-17 08:44:41 +0100

    Update vendorsetup.sh
    17/03/26

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Infinix-X6886-user
-add_lunch_combo omni_Infinix-X6886-userdebug
-add_lunch_combo omni_Infinix-X6886-eng
+add_lunch_combo twrp_Infinix-X6886-user
+add_lunch_combo twrp_Infinix-X6886-userdebug
+add_lunch_combo twrp_Infinix-X6886-eng
